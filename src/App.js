2440184e8bb62e146f85fcf6d8941a143e22193f
alexjamesmx <sg.alex32@gmail.com>
2024-08-18 09:23:30 -0600

    authentication, history

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { toast, ToastContainer } from "react-toastify";
 import { Carousel } from "react-responsive-carousel";
+import AuthModal from "./components/signup";
 
 function SearchPage() {
-  // Function to generate a random color
+  const [activeTab, setActiveTab] = useState("scrape"); // Tab state
+  // const BASE_URL = "https://guarded-retreat-97709-6840d092ecf7.herokuapp.com";
+
+  const BASE_URL = "http://localhost:3001";
+  const [labels, setLabels] = useState([]);
+  const [summary, setSummary] = useState("");
+  const [history, setHistory] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [user, setUser] = useState("");
+  const [url, setUrl] = useState("");
+  const [websiteName, setWebsiteName] = useState("");
+  const [keyPoints, setKeyPoints] = useState([]);
+  const [historyRefresher, setHistoryRefresher] = useState(false);
+  const [images, setImages] = useState([]);
+  const [useFullLinks, setUseFullLinks] = useState(false);
+  const [relatedContent, setRelatedContent] = useState([]);
+  const [fetchingSummary, setFetchingSummary] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   const generateRandomColor = () => {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -14,26 +35,68 @@ function SearchPage() {
     return color;
   };
 
-  // const BASE_URL = "http://localhost:3001";
-  const BASE_URL = "https://guarded-retreat-97709-6840d092ecf7.herokuapp.com";
-  const [labels, setLabels] = useState([]);
-  const [summary, setSummary] = useState("");
-  const [url, setUrl] = useState("");
-  const [websiteName, setWebsiteName] = useState("");
-  const [keyPoints, setKeyPoints] = useState([]);
-  const [images, setImages] = useState([]);
-  const [useFullLinks, setUseFullLinks] = useState(false);
-  const [relatedContent, setRelatedContent] = useState([]);
-  const [fetchingSummary, setFetchingSummary] = useState(false);
+  // On the client side (React)
+  const handleLogout = () => {
+    localStorage.removeItem("authToken"); // Remove the JWT token
+    // Optionally, you can redirect the user to the login page
+    window.location.href = "/login";
+  };
+
+  const getHistory = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/scrape/history`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        toast.error("There was a problem with history fetch operation");
+        return;
+      }
+      const data = await response.json();
+      setHistory(data);
+      console.log(data);
+    } catch (error) {
+      console.error(
+        "There was a problem with the history fetch operation:",
+        error
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      setUser(localStorage.getItem("authToken"));
+    }
+  }, [isModalOpen]);
+
+  useEffect(() => {
+    if (!user) return;
+    console.log("getting history");
+    getHistory();
+  }, [user, historyRefresher]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!user) {
+      openModal();
+      return;
+    }
+
+    if (!url) {
+      alert("Please enter a valid URL");
+      return;
+    }
+
     setFetchingSummary(true);
     try {
       const response = await fetch(`${BASE_URL}/scrape`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
         },
         body: JSON.stringify({ url }),
       });
@@ -49,7 +112,6 @@ function SearchPage() {
       setSummary(website.summary);
       setUseFullLinks(website.useFullLinks);
 
-      // Assign random colors to each label
       const labeledColors = website.labels.map((label) => ({
         text: label,
         color: generateRandomColor(),
@@ -60,12 +122,37 @@ function SearchPage() {
       setRelatedContent(website.relatedContent);
       setKeyPoints(website.keyPoints);
       setImages(website.images);
+      setHistoryRefresher(!historyRefresher);
     } catch (error) {
       console.error("There was a problem with the fetch operation:", error);
     } finally {
       setFetchingSummary(false);
     }
   };
+
+  const clearHistory = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/scrape/history`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        toast.error("There was a problem with the history clear operation");
+        return;
+      }
+      toast.success("History cleared successfully");
+      setHistory([]);
+    } catch (error) {
+      console.error(
+        "There was a problem with the history clear operation:",
+        error
+      );
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 p-4">
@@ -84,138 +171,223 @@ function SearchPage() {
         <h1 className="text-4xl font-semibold text-gray-800 mb-8">
           Easy Web Scraping
         </h1>
-        <form
-          className="w-full max-w-lg flex items-center mb-8"
-          onSubmit={handleSubmit}
-        >
-          <input
-            type="text"
-            className="flex-grow px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter the URL for webscraping"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            disabled={fetchingSummary}
-          />
+
+        {/* Tabs */}
+        <div className="flex space-x-4 mb-8">
           <button
-            type="submit"
-            className="ml-4 px-6 py-2 text-white bg-[#538392] rounded-lg shadow-md hover:bg-[#3e5b65] transition-all duration-300"
+            className={`px-4 py-2 rounded-lg shadow-md ${
+              activeTab === "scrape"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-300 text-gray-700"
+            }`}
+            onClick={() => setActiveTab("scrape")}
           >
-            {fetchingSummary ? <div className="loader"></div> : "Search"}
+            Scrape
           </button>
-        </form>
-        <div className="w-full max-w-lg bg-white p-6 rounded-lg shadow-lg">
-          {summary || labels.length || keyPoints.length ? (
-            <h2 className="text-xl font-semibold text-gray-800 mb-4">
-              Summary of the website: {websiteName}
-            </h2>
-          ) : (
-            <h2 className="text-xl text-gray-800 mb-4">
-              Start searching by entering a URL
-            </h2>
+
+          {user && (
+            <button
+              className={`px-4 py-2 rounded-lg shadow-md ${
+                activeTab === "history"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-300 text-gray-700"
+              }`}
+              onClick={() => setActiveTab("history")}
+            >
+              History
+            </button>
           )}
-          <p className="text-lg text-gray-700 mb-4">{summary}</p>
-          {labels.length > 0 && (
-            <div className="flex flex-wrap gap-2">
-              {labels.map((label, index) => (
-                <span
-                  key={index}
-                  className="px-3 py-1 text-white rounded-full"
-                  style={{ backgroundColor: label.color }}
-                >
-                  {label.text}
-                </span>
-              ))}
-            </div>
-          )}
-          {keyPoints.length > 0 && (
-            <>
-              <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
-                Key Points
-              </h2>
-              <ul className="list-disc list-inside text-gray-700">
-                {keyPoints.map(({ title, value }, index) => (
-                  <li key={index}>
-                    <span className="font-semibold">{title}: </span>
-                    {value}
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {useFullLinks.length > 0 && (
-            <>
-              <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
-                Useful Links
-              </h2>
-              <ul className="list-disc list-inside text-gray-700">
-                {useFullLinks.map(({ title, url }, index) => (
-                  <li key={index}>
-                    <span className="font-semibold">{title}: </span>
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="text-blue-500 break-all"
-                      style={{
-                        wordBreak: "break-all",
-                        overflowWrap: "break-word",
-                      }}
-                    >
-                      {url}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {relatedContent.length > 0 && (
-            <>
-              <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
-                Related Content
-              </h2>
-              <ul className="list-disc list-inside text-gray-700">
-                {relatedContent.map(({ title, url }, index) => (
-                  <li key={index}>
-                    <span className="font-semibold">{title}: </span>
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="text-blue-500 break-all"
-                      style={{
-                        wordBreak: "break-all",
-                        overflowWrap: "break-word",
-                      }}
-                    >
-                      {url}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {images.length > 0 && (
-            <>
-              <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
-                Images
-              </h2>
-              <Carousel showThumbs={false} infiniteLoop={true}>
-                {images.map((image, index) => (
-                  <div key={index}>
-                    <img
-                      src={image.src}
-                      alt={image.alt || "Image"}
-                      width={"200px"}
-                      height={"200px"}
-                    />
-                    {image.alt && <p className="legend">{image.alt}</p>}
-                  </div>
-                ))}
-              </Carousel>
-            </>
+
+          {user && (
+            <button
+              className="px-4 py-2 rounded-lg shadow-md bg-red-500 text-white"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
           )}
         </div>
+
+        {/* Tab Content */}
+        {activeTab === "scrape" && (
+          <div className="w-full max-w-lg">
+            <form
+              className="w-full max-w-lg flex items-center mb-8"
+              onSubmit={handleSubmit}
+            >
+              <input
+                type="text"
+                className="flex-grow px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter the URL for webscraping"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                disabled={fetchingSummary}
+              />
+              <button
+                type="submit"
+                className="ml-4 px-6 py-2 text-white bg-[#538392] rounded-lg shadow-md hover:bg-[#3e5b65] transition-all duration-300"
+              >
+                {fetchingSummary ? <div className="loader"></div> : "Search"}
+              </button>
+            </form>
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              {summary || labels.length || keyPoints.length ? (
+                <h2 className="text-xl font-semibold text-gray-800 mb-4">
+                  Summary of the website: {websiteName}
+                </h2>
+              ) : (
+                <h2 className="text-xl text-gray-800 mb-4">
+                  Start searching by entering a URL
+                </h2>
+              )}
+
+              <p className="text-lg text-gray-700 mb-4">{summary}</p>
+              {labels.length > 0 && (
+                <div className="flex flex-wrap gap-2">
+                  {labels.map((label, index) => (
+                    <span
+                      key={index}
+                      className="px-3 py-1 text-white rounded-full"
+                      style={{ backgroundColor: label.color }}
+                    >
+                      {label.text}
+                    </span>
+                  ))}
+                </div>
+              )}
+              {keyPoints.length > 0 && (
+                <>
+                  <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
+                    Key Points
+                  </h2>
+                  <ul className="list-disc list-inside text-gray-700">
+                    {keyPoints.map(({ title, value }, index) => (
+                      <li key={index}>
+                        <span className="font-semibold">{title}: </span>
+                        {value}
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+              {useFullLinks.length > 0 && (
+                <>
+                  <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
+                    Useful Links
+                  </h2>
+                  <ul className="list-disc list-inside text-gray-700">
+                    {useFullLinks.map(({ title, url }, index) => (
+                      <li key={index}>
+                        <span className="font-semibold">{title}: </span>
+                        <a
+                          href={url}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-blue-500 break-all"
+                          style={{
+                            wordBreak: "break-all",
+                            overflowWrap: "break-word",
+                          }}
+                        >
+                          {url}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+              {relatedContent.length > 0 && (
+                <>
+                  <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
+                    Related Content
+                  </h2>
+                  <ul className="list-disc list-inside text-gray-700">
+                    {relatedContent.map(({ title, url }, index) => (
+                      <li key={index}>
+                        <span className="font-semibold">{title}: </span>
+                        <a
+                          href={url}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-blue-500 break-all"
+                          style={{
+                            wordBreak: "break-all",
+                            overflowWrap: "break-word",
+                          }}
+                        >
+                          {url}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+              {images.length > 0 && (
+                <>
+                  <h2 className="text-xl font-semibold text-gray-800 mt-6 mb-4">
+                    Images
+                  </h2>
+                  <Carousel showThumbs={false} infiniteLoop={true}>
+                    {images.map((image, index) => (
+                      <div key={index}>
+                        <img
+                          src={image.src}
+                          alt={image.alt || "Image"}
+                          width={"200px"}
+                          height={"200px"}
+                          style={{
+                            objectFit: "cover",
+                            width: "200px",
+                            height: "200px",
+                          }}
+                        />
+                        {image.alt && <p className="legend">{image.alt}</p>}
+                      </div>
+                    ))}
+                  </Carousel>
+                </>
+              )}
+            </div>
+          </div>
+        )}
+
+        {activeTab === "history" && (
+          <div className="w-full max-w-lg bg-white p-6 rounded-lg shadow-lg">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-xl font-semibold text-gray-800 mb-4">
+                Search History
+              </h2>
+              <button
+                className="px-4 py-2 text-white bg-[#538392] rounded-lg shadow-md hover:bg-[#3e5b65] transition-all duration-300"
+                onClick={clearHistory}
+              >
+                Clear
+              </button>
+            </div>
+
+            {history.length > 0 ? (
+              <ul className="list-disc list-inside text-blue-500">
+                {history.map((item, index) => (
+                  <li
+                    key={index}
+                    className="cursor-pointer"
+                    onClick={() => {
+                      setActiveTab("scrape");
+                      setUrl(item.websiteLink);
+                      console.log("item", item.website.website);
+                    }}
+                  >
+                    <span className="font-semibold">{item.websiteLink}: </span>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-gray-700">No history available</p>
+            )}
+          </div>
+        )}
       </div>
+      <AuthModal isOpen={isModalOpen} onClose={closeModal} />
     </>
   );
 }
---
+++ src/components/signup.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+
+function AuthModal({ isOpen, onClose }) {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const BASE_URL = "http://localhost:3001/auth";
+
+  const toggleForm = () => setIsLogin(!isLogin);
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      const payload = { email: values.email, password: values.password };
+
+      try {
+        const response = await fetch(
+          `${BASE_URL}/${isLogin ? "login" : "register"}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+          }
+        );
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          toast.error(data.message || "There was a problem with the operation");
+          return;
+        }
+
+        toast.success(`Successfully ${isLogin ? "logged in" : "signed up"}!`);
+        console.log("Token:", data.token);
+        console.log("User:", data.user);
+
+        localStorage.setItem("authToken", data.token);
+        onClose();
+      } catch (error) {
+        console.error("There was a problem with the operation:", error);
+        toast.error("An error occurred. Please try again.");
+      }
+    },
+  });
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-black opacity-50 fixed inset-0"></div>
+          <div className="bg-white p-6 rounded-lg shadow-lg z-10 max-w-md w-full">
+            <h2 className="text-2xl font-semibold mb-4">
+              {isLogin ? "Login" : "Sign Up"}
+            </h2>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="mb-4">
+                <label className="block text-gray-700">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="w-full p-2 border border-gray-300 rounded-lg"
+                  placeholder="Enter your email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="text-red-500">{formik.errors.email}</div>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="w-full p-2 border border-gray-300 rounded-lg"
+                  placeholder="Enter your password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="text-red-500">{formik.errors.password}</div>
+                ) : null}
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
+              >
+                {isLogin ? "Login" : "Sign Up"}
+              </button>
+            </form>
+            <p className="text-center mt-4">
+              {isLogin ? "Don't have an account?" : "Already have an account?"}{" "}
+              <button
+                type="button"
+                className="text-blue-500 underline"
+                onClick={toggleForm}
+              >
+                {isLogin ? "Sign Up" : "Login"}
+              </button>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default AuthModal;
