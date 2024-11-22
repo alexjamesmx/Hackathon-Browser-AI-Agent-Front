efe1e34519b267e8d177854f2189b43234ed61eb
alexjamesmx <sg.alex32@gmail.com>
2024-11-22 13:35:50 -0600

    modal updated

--- src/components/signup.js
+++ src/components/signup.js
@@ -73,9 +73,23 @@ function AuthModal({ isOpen, onClose }) {
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="bg-black opacity-50 fixed inset-0"></div>
           <div className="bg-white p-6 rounded-lg shadow-lg z-10 max-w-md w-full">
-            <h2 className="text-2xl font-semibold mb-4">
-              {isLogin ? "Login" : "Sign Up"}
-            </h2>
+            <div className="flex justify-between">
+              <h2 className="text-2xl font-semibold mb-4">
+                {isLogin ? "Login" : "Sign Up"}
+              </h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-x-lg"
+                viewBox="0 0 16 16"
+                onClick={() => onClose()}
+              >
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+              </svg>
+            </div>
+
             <form onSubmit={formik.handleSubmit}>
               <div className="mb-4">
                 <label className="block text-gray-700">Email</label>
